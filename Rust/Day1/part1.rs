aef3a8b6cc985a400727947792b987fa2759b4cd
Joel Bonini <joel.bonini@gmx.ch>
2025-12-03 16:08:16 +0100

    Day1 part1 Rust done

--- Rust/Day1/part1.rs
+++ Rust/Day1/part1.rs
@@ -1,11 +1,44 @@
-use std::fs::File;
+use std::fs;
 
 
-fn parse() -> Vec<[&str, i32]> {
-    let mut rotations: Vec<[&str, i32]> = Vec::new();
-    let mut file = File::open("./input.txt")?;
 
+fn parse() -> Vec<(String, i32)> {
+    let mut rotations: Vec<(String, i32)> = Vec::new();
+    let file_contents = fs::read_to_string("./input.txt").expect("Failed to read file.");
+    let lines: Vec<String> = file_contents.lines().map(|s| s.to_string()).collect();
 
+    for line in lines {
+        let key: &str = &line[0..1];
+        let value: &str = &line[1..];
+        let value_int: i32 = value.parse().expect("Failed to parse to int.");
+        rotations.push((key.to_string(), value_int));
+    }
 
     return rotations;
+}
+
+
+
+fn main() {
+    let rotations = parse();
+    let dial: Vec<u32> = (0..100).collect();
+    let mut counter: u32 = 0;
+    let mut current_pos: i32 = 50;
+
+    for (dir, amount) in rotations {
+        if dir == "L" {
+            current_pos -= amount;
+        } else {
+            current_pos += amount;
+        }
+
+        current_pos = current_pos.rem_euclid(100);
+
+        if dial[current_pos as usize] == 0 {
+            counter += 1;
+        }
+    }
+
+    println!("{}", counter);
+
 }
